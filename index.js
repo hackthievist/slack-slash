b25cecc56f60de221b382ed3a7c95c877bef3d26
Ifeoluwa Sobogun <ifeoluwa@paystack.com>
2020-06-20 08:35:15 +0100

    debug

--- index.js
+++ index.js
@@ -8,17 +8,19 @@ app.use(bodyParser({ extended: false }));
 app.get('/', (req, res) => res.send('Haha what are you looking for?'));
 
 app.post('/get-gif', async (req, res) => {
+    console.log(req.body);
     const urls = [
         'https://res.cloudinary.com/dlz2qvds4/image/upload/v1592592702/samples/khadijah/Animated_GIF-downsized_large_2.gif',
         'https://res.cloudinary.com/dlz2qvds4/image/upload/v1592592765/samples/khadijah/Animated_GIF-downsized_large_1.gif',
     ];
     const randomIndex = Math.floor(Math.random() * urls.length);
     const responseUrl = req.body.response_url;
-    await fetch(responseUrl, {
+    const response = await fetch(responseUrl, {
         method: 'post',
         body:    JSON.stringify({ delete_original: true }),
         headers: { 'Content-Type': 'application/json' },
     });
+    console.log(response);
     return res.status(200).send({
         response_type: 'in_channel',
         text: `<${urls[randomIndex]}| good job>`,
